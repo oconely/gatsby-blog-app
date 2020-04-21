4618e796a2e0b56eb00bea03a74a49460e1ce169
Andrew Rybaltouski <andrew.rybaltouski@idt.net>
2020-04-21 16:46:07 +0300

    querying siteMetadata with graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion']
+  siteMetadata: {
+    title: `Gatsby Blog App`,
+    siteUrl: `http:/localhost:8000`,
+    description: `Blazing fast modern site generator for React`,
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet']
 }
