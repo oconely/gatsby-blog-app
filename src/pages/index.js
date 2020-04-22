a5b758692b7aefbeaaac66deb4d7956c3738ffca
Andrew Rybaltouski <andrew.rybaltouski@idt.net>
2020-04-22 20:51:18 +0300

    added Hero banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Hero from '../components/hero';
 import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/posts-preview';
@@ -8,15 +9,14 @@ export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello there!</p>
-      <Link to='/about/'>Learn about &rarr;</Link>
-
-      <h2>Read my blog</h2>
-      {posts.map((post) => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map((post) => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
